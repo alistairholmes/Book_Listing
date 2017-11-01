925b4e596c1bb9bebf77af7e67117c1d7180d96d
alistairholmes <alistairholmes95@gmail.com>
2017-11-01 14:39:32 +0200

    Created the Book class, and made an object for necessary params.

--- app/src/main/java/com/example/alistair/booklistingapp/Book.java
+++ app/src/main/java/com/example/alistair/booklistingapp/Book.java
@@ -1,12 +1,47 @@
 package com.example.alistair.booklistingapp;
 
 public class Book {
+
+    // Create variables for the book info constructor
+
+    // Title of the book
+    private String mTitle;
+
+    // Author of the book
+    private String mAuthor;
+
+    // Image of the book ( url )
+    private String mImage;
+
+    // Url to book webpage
+    private String mUrl;
+
+    // Create constructor accepting variables above
+    public Book(String title, String author, String image) {
+        mTitle = title;
+        mAuthor = author;
+        mImage = image;
+    }
+
+    // Make getter methods for those variables
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
 }
 
-// Todo Create variables for the book info constructor
 
 
-// Todo Create constructor accepting variables above
 
 
-// Todo Make getter methods for those variables
+
+
+
