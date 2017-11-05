f24f6c0af0fff974eec38871f1ebf6a226306364
alistairholmes <alistairholmes95@gmail.com>
2017-11-05 21:15:00 +0200

    Parse that JSON yo!

--- app/src/main/java/com/example/alistair/booklistingapp/QueryUtils.java
+++ app/src/main/java/com/example/alistair/booklistingapp/QueryUtils.java
@@ -4,11 +4,19 @@ package com.example.alistair.booklistingapp;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 public class QueryUtils {
@@ -24,10 +32,8 @@ public class QueryUtils {
     private QueryUtils() {
     }
 
-/*
-    */
-/*//*
-/ The app fetches book data related to the query via an HTTP request from the Google Books API,
+
+// The app fetches book data related to the query via an HTTP request from the Google Books API,
     public static ArrayList<Book> fetchBookData(String requestUrl) {
         // Create URL object
         URL url = createUrl(requestUrl);
@@ -41,14 +47,14 @@ public class QueryUtils {
         }
 
         // Extract relevant fields from the JSON response and create a list of {@link Book}s
-        List<Book> books =extractFeatureFromJson(jsonResponse);
+        ArrayList<Book> books =extractFeatureFromJson(jsonResponse);
 
         // Return the list of {@link Earthquake}s
-        return books;*//*
+        return books;
 
 
     }
-*/
+
     // Returns new URL object from the given string URL.
     private static URL createUrl(String stringUrl) {
         URL url = null;
@@ -61,7 +67,7 @@ public class QueryUtils {
     }
 
     // Make an HTTP request to the given URL and return a String as the response.
-    private static String makeHttpUrlRequest(URL url) throws IOException {
+    private static String makeHttpRequest(URL url) throws IOException {
         String jsonResponse = "";
 
         // Return early if the url is null
@@ -84,15 +90,23 @@ public class QueryUtils {
             // then read the input stream and parse the response.
             if (urlConnection.getResponseCode() == 200) {
                 Log.d(LOG_TAG, "TEST: Connection to server was successful!");
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
             } else {
                 Log.e(LOG_TAG, "TEST: Unable to connect to server" + urlConnection.getResponseCode());
             }
         } catch (IOException e) {
-            Log.e(LOG_TAG, "TEST: Error in retreiving book json information.");
+            Log.e(LOG_TAG, "TEST: Error in retrieving book json information.");
         } finally {
             // The result of the request is validated to account for a bad server response or lack of server response.
             if (urlConnection != null) {
                 urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
             }
         }
 
@@ -100,11 +114,78 @@ public class QueryUtils {
 
     }
 
+    /* Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Return a list of {@link Book} objects that has been built up from
+     * parsing the given JSON response.
+     */
+    private static ArrayList<Book> extractFeatureFromJson(String bookJSON) {
+
+        // Create an empty ArrayList that we can start adding earthquakes to
+        ArrayList<Book> books = new ArrayList<>();
+
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(bookJSON);
+
+            // Extract the JSONArray associated with the key called "items",
+            // which represents a list of items (or books).
+            JSONArray bookItemsArray = baseJsonResponse.getJSONArray("items");
+
+            for (int i = 0; i < bookItemsArray.length(); i++) {
+
+                // Get a single book at position i within the list of books
+                JSONObject currentBook = bookItemsArray.getJSONObject(i);
+
+                // For a given earthquake, extract the JSONObject associated with the
+                // key called "properties", which represents a list of all properties
+                // for that earthquake.
+                JSONObject volumeInfo = currentBook.getJSONObject("volumeInfo");
+
+                // Extract the value for the key called "title"
+                String title = volumeInfo.getString("title");
+
+                // Extract the value for the key called "authors"
+                String author = volumeInfo.getString("authors");
+
+                // Create a new {@link Book} object with the titlw and author from the JSON response.
+                Book book = new Book(title, author);
+
+                // Add the new {link Book} to the books list
+                books.add(book);
+            }
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the book JSON results");
+        }
+
+        return books;
+    }
 
 
 
-
-    // Todo The JSON response is parsed correctly, and relevant information is stored in the app.
 
     // Todo The ListView is properly populated with the information parsed from the JSON response.
 
